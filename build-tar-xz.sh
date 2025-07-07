f1b0a7678e955791a63ec1757b321b51be2cf47d
marcbull <31337222+marcbull@users.noreply.github.com>
2025-07-07 11:53:35 +0200

    chore: update to latest sancus version

--- build-tar-xz.sh
+++ build-tar-xz.sh
@@ -49,7 +49,7 @@ main() {
 
 	${SCRIPT_DIR}/check-build-env.sh
 
-	cargo install --version 0.1.0 sancus
+	cargo install --version 0.1.3 sancus
 
 	local VERSION="$(set -e;cargo version-util get-version)"
 	local TAR_PACKAGE_NAME="${PACKAGE}-${VERSION}-$(set -e;${SCRIPT_DIR}/get-vcpkg-triplet.py).tar.xz"
